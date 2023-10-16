d98c2a3cedb7af0965fddd153978ac79ba11c1c3
arefir <afirdaus.razak11@gmail.com>
2023-10-16 17:12:49 +0900

    mongoose connect method change

--- config/db.js
+++ config/db.js
@@ -3,12 +3,8 @@ const { MONGO_URI } = require("./config");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      //   useCreateIndex: true,
-      //   useFindAndModify: false,
-    });
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(MONGO_URI);
 
     console.log("Connected to MongoDB");
   } catch (err) {
