ca5c0a7f922d268d1512ac103cca989bb45a99bd
arefir <afirdaus.razak11@gmail.com>
2023-10-18 00:53:19 +0900

    무장애 정보 api

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ export const env = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
   API_KEY: process.env.API_KEY,
+  API_KEY2: process.env.API_KEY2,
   ACCESS_KEY: process.env.ACCESS_KEY,
   SECRET_KEY: process.env.SECRET_KEY,
 };
--- routes/api/facilities.js
+++ routes/api/facilities.js
@@ -3,19 +3,40 @@ import axios from "axios";
 import convert from "xml-js";
 import xmlParser from "xml2json";
 import { env } from "../../config/config.js";
+import url from "url";
 
-const { API_KEY } = env;
+const { API_KEY, API_KEY2 } = env;
 const router = express.Router();
 
 router.get("/", async (req, res) => {
+  if (req.query.x == null || req.query.y == null)
+    return res.json({ msg: "Invalid query" });
+
+  const { x, y } = req.query;
+
   try {
+    let payload = {
+      numOfRows: 1000,
+      MobileOS: "WIN",
+      MobileApp: "travelHelper",
+      mapX: x,
+      mapY: y,
+      radius: 5000,
+      serviceKey: API_KEY,
+    };
+
+    const params = new url.URLSearchParams(payload);
+
     const apiRes = await axios.get(
-      `https://apis.data.go.kr/B551011/KorWithService1/locationBasedList1?MobileOS=WIN&MobileApp=travelHelper&mapX=${x}&mapY=${y}.5393463&radius=5000&serviceKey=${API_KEY}`
+      `https://apis.data.go.kr/B551011/KorWithService1/locationBasedList1?${params}`
     );
+
+    const parsedURL = url.parse(apiRes.config.url);
+    // console.log(parsedURL);
     // console.log(apiRes.data);
     var result = JSON.parse(xmlParser.toJson(apiRes.data));
-    console.log(result);
-    res.json(result);
+    // console.log(apiRes);
+    res.json(result.response.body);
   } catch (err) {
     const date = new Date(Date.now());
     console.error(`${err} (${date.toLocaleString()})`);
