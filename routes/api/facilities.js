c3458477c01709d8c42987a37a2a482919769fc2
arefir <afirdaus.razak11@gmail.com>
2023-10-18 01:11:22 +0900

    remove unrequired items

--- routes/api/facilities.js
+++ routes/api/facilities.js
@@ -1,6 +1,5 @@
 import express from "express";
 import axios from "axios";
-import convert from "xml-js";
 import xmlParser from "xml2json";
 import { env } from "../../config/config.js";
 import url from "url";
