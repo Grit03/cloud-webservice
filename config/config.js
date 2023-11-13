c6620c31b776e46ddafcf736ba64f6a0cd5bc28b
arefir <afirdaus.razak11@gmail.com>
2023-11-14 07:24:49 +0900

    new branch initial commit

--- config/config.js
+++
@@ -1,9 +0,0 @@
-export const env = {
-  NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT,
-  MONGO_URI: process.env.MONGO_URI,
-  API_KEY: process.env.API_KEY,
-  API_KEY2: process.env.API_KEY2,
-  ACCESS_KEY: process.env.ACCESS_KEY,
-  SECRET_KEY: process.env.SECRET_KEY,
-};
